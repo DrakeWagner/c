931e0edc104d89d6a85295cae93f520588893db3
DrakeWagner <dbw2tn@virginia.edu>
2020-10-09 19:55:23 -0400

    add input and variable files

---
+++ variable_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	
+	// define variable
+	char first_a = 'a';
+
+	// this variable is declared with no definition
+	// well, technically the definition is assigned a garbage value
+	float b;
+
+	// declare and define multiple on one line
+	int _c, _d, e;
+
+	// print variable
+	printf("%c \n", first_a);
+
+	return 0;
+}
