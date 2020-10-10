34726d72a8d930100ffde7b3ca631ec4815cab37
DrakeWagner <dbw2tn@virginia.edu>
2020-10-10 11:09:29 -0400

    update

---
+++ input_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char name[20];
+
+	printf("Please enter your name: ");
+	scanf("%s", name);
+
+	printf("\nName: %s", name);
+	printf("\n");
+
+	return 0;
+
+}	
